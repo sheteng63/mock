db4b901ca1d4ede4d074dec80794e50ea3ae8b26
sheteng <sheteng@pi2e.com>
2017-10-25 18:42:05 +0800

    添加一些数据库操作

--- PythonMock/PythonMock/mockdb.py
+++ PythonMock/PythonMock/mockdb.py
@@ -4,8 +4,36 @@ from mockapi.models import MockData
 import os
 
 # 数据库操作
-def testdb(request):
-    test1 = MockData(urlname=request.GET["urlName"])
+#添加
+def addDb(request):
+
+    test = MockData(urlname=request.GET["urlName"])
     print("urlName = %s "  % request.GET["urlName"])
-    test1.save()
+    print("path = %s "  % request.path)
+    test.save()
     return HttpResponse("<p>数据添加成功！</p>")
+#修改
+def updateDb(request):
+    test = MockData.object.get(id = request.GET['id'])
+    test.urlname = request.GET["urlName"]
+    test.urlType = request.GET["urlType"]
+    test.urlResponse = request.GET["urlResponse"]
+    test.save()
+    return HttpResponse("<p>数据修改成功！</p>")
+
+#删除
+def deleteDb(request):
+    test = MockData.object.get(id = request.GET['id'])
+    test.delete()
+    return HttpResponse("<p>删除成功</p>")
+
+#查询所有
+def queryAll(request):
+    list = MockData.object.all()
+    print("lsit = %s" % list)
+
+#
+def query(request):
+    response = MockData.object.get(urlname = request.path)
+    res = response.urlResponse
+    return HttpResponse("<p>查询成功</p>")
--- PythonMock/PythonMock/urls.py
+++ PythonMock/PythonMock/urls.py
@@ -3,6 +3,9 @@ from . import view
 from . import mockdb
 
 urlpatterns = [
-    url(r'^$',view.hello),
-    url(r'^addNewUrl$', mockdb.testdb)
+    url(r'^',mockdb.query),
+    url(r'^addNewUrl$', mockdb.addDb),
+    url(r'^updateUrl$',mockdb.updateDb),
+    url(r'^deleteUrl$',mockdb.deleteDb),
+    url(r'^queryUrl$', mockdb.queryAll)
 ]
